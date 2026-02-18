2ba9fa67cedfbbf791c36279ca84a930cd495073
H6LS1S <seagull.bogdan@gmail.com>
2026-02-19 00:22:19 +0200

    feat: added wifi provisioning btn

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { mdiBrightness3, mdiBrightness4, mdiUpload } from '@mdi/js';
+	import { mdiBrightness3, mdiBrightness4, mdiUpload, mdiWifiCog } from '@mdi/js';
 	import { onMount } from 'svelte';
 
 	import { ApiService } from '@/services/api';
@@ -9,15 +9,16 @@
 	const api = new ApiService();
 
 	let theme = $state(localStorage.getItem('theme') === 'dark');
+	let connected = $state(true);
 	let fileInput: HTMLInputElement;
 
-	const toggleTheme = async () => {
+	const changeTheme = async () => {
 		theme = !theme;
 		document.documentElement.classList.toggle('dark', theme);
 		localStorage.setItem('theme', theme ? 'dark' : 'light');
 	};
 
-	const updateFirmware = async (): Promise<void> => {
+	const changeFirmware = async (): Promise<void> => {
 		fileInput.click();
 
 		const { target } = await new Promise<Event>((resolve) => {
@@ -28,26 +29,46 @@
 		const [file] = (target as HTMLInputElement).files || [];
 		if (!file) return;
 		await api
-			.updateFirmware(file)
-			.then(() => setInterval(() => location.reload(), 2000))
+			.changeFirmware(file)
+			.then(() => setInterval(() => location.reload(), 5000))
 			.finally(() => (fileInput.value = ''));
 	};
 
-	onMount(() => {
+	const changeWifiMode = async () => {
+		const ssid = window.prompt('SSID');
+		const passphrase = window.prompt('PASS') || '';
+
+		if (!ssid) return;
+
+		await api.setProvision(ssid, passphrase).then(() => api.applyProvision());
+		await new Promise((_, reject) =>
+			setInterval(() => {
+				api
+					.getProvisionStatus()
+					.then(({ respGetStatus: res }) => !res?.staState && location.reload())
+					.catch(reject);
+			}, 5000)
+		);
+	};
+
+	onMount(async () => {
 		document.documentElement.classList.toggle('dark', theme);
-		window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', toggleTheme);
+		window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', changeTheme);
+
+		await api.getProvisionStatus().then(({ respGetStatus: res }) => (connected = !res?.staState));
 	});
 </script>
 
 <header class="flex items-center px-8 pt-4">
 	<h1 class="text-3xl mr-auto">WebUART:{__APP_VERSION__}</h1>
 
-	<Btn click={updateFirmware} icon={mdiUpload} />
-	<Btn click={toggleTheme} icon={theme ? mdiBrightness4 : mdiBrightness3} />
-
-	<input type="file" accept=".bin" hidden bind:this={fileInput} />
+	<Btn click={changeFirmware} icon={mdiUpload} />
+	<Btn click={changeTheme} icon={theme ? mdiBrightness4 : mdiBrightness3} />
+	<Btn click={changeWifiMode} icon={mdiWifiCog} hidden={connected} />
 </header>
 
 <main class="container m-auto my-4 grid grid-rows-[auto_1fr] rounded bg-gray-200 dark:bg-black">
 	<Console />
 </main>
+
+<input type="file" accept=".bin" hidden bind:this={fileInput} />
